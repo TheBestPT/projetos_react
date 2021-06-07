3ae675e70c5013b051f0e12152111de4c5fc830f
TheBestPT <72525612+TheBestPT@users.noreply.github.com>
2021-06-07 12:17:12 +0100

    2 examples.

---
+++ projetos_escola/src/components/Aplicacao.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export default class Aplicacao extends React.Component{
+     render(){
+         return <h2>Via Componente Aplicacao!</h2>
+     }
+}
+
--- projetos_escola/src/index.js
+++ projetos_escola/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import Aplicacao from './components/Aplicacao'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+ReactDom.render(
+    <h1>Ola, via render index.js</h1>,
+    document.getElementById('aplicacao')
+)*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(
+    <Aplicacao/>,
+    document.getElementById('aplicacao')
+)
