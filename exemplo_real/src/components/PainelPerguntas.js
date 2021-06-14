da5adfd6954045d5d1d335511c97a1ede4dcbf5e
TheBestPT <72525612+TheBestPT@users.noreply.github.com>
2021-06-14 20:26:04 +0100

    Select the answer.

--- exemplo_real/src/components/PainelPerguntas.js
+++ exemplo_real/src/components/PainelPerguntas.js
@@ -7,7 +7,8 @@ export default class PainelPerguntas extends React.Component {
         super()
         this.state = {
             currentQuestion : 0,
-            images : this.importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/))
+            images : this.importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/)),
+            answerSelected : []
         }
     }
     render(){
@@ -19,16 +20,16 @@ export default class PainelPerguntas extends React.Component {
                 <img src={this.state.images[Questions.questions[this.state.currentQuestion].question_image].default} />
                 <h3><b>{this.state.currentQuestion + 1} - {Questions.questions[this.state.currentQuestion].question}</b></h3>
                 <h4>Possible Answer</h4>
-                <div className="answer" onClick={this.selectQuestion.bind(this)}>
+                <div className="answer" id="1Question" onClick={this.selectQuestion.bind(this)}>
                     <p><b>A - </b>{Questions.questions[this.state.currentQuestion].possible_answer[0]}</p>
                 </div>
-                <div className="answer" onClick={this.selectQuestion.bind(this)}>
+                <div className="answer" id="2Question" onClick={this.selectQuestion.bind(this)}>
                     <p><b>B - </b>{Questions.questions[this.state.currentQuestion].possible_answer[1]}</p>
                 </div>
-                <div className="answer" onClick={this.selectQuestion.bind(this)}>
+                <div className="answer" id="3Question" onClick={this.selectQuestion.bind(this)}>
                     <p><b>C - </b>{Questions.questions[this.state.currentQuestion].possible_answer[2]}</p>
                 </div>
-                <div className="answer" onClick={this.selectQuestion.bind(this)}>
+                <div className="answer" id="4Question" onClick={this.selectQuestion.bind(this)}>
                     <p><b>D - </b>{Questions.questions[this.state.currentQuestion].possible_answer[3]}</p>
                 </div>
                 <button onClick={this.nextQuestion.bind(this)}>Next</button>
@@ -48,6 +49,28 @@ export default class PainelPerguntas extends React.Component {
     }
 
     selectQuestion(selector){
-        selector.target.style.background = 'black'
+        //this.setState({answerSelected : })
+        //!selector.target.childNodes[1] ? selector.target.classList.add('answer_selected') : selector.target.parentElement.classList.add('answer_selected')
+        !selector.target.childNodes[1] ? selector.target.classList.contains('answer_selected') ? selector.target.classList.remove('answer_selected') : selector.target.classList.add('answer_selected') : selector.target.parentElement.classList.contains('answer_selected') ? selector.target.parentElement.classList.remove('answer_selected') : selector.target.parentElement.classList.add('answer_selected')
+        if(selector.target.id) {
+            //this.setState({answerSelected : selector.target.id })
+            let id = selector.target.id
+            id = id.replace('Question', '')
+            let question = this.state.currentQuestion
+            if(!this.state.answerSelected[question])
+                this.state.answerSelected.push(id)
+            else
+                this.state.answerSelected[question] = id
+            console.log(this.state.answerSelected)
+        }else{
+            let id = selector.target.parentElement.id
+            id = id.replace('Question', '')
+            let question = this.state.currentQuestion
+            if(!this.state.answerSelected[question])
+                this.state.answerSelected.push(id)
+            else
+                this.state.answerSelected[question] = id
+        }
+        console.log(this.state.answerSelected)
     }
 }
