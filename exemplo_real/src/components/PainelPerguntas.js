5828d258280c0beabe628203a6477d6dcc7f32aa
TheBestPT <72525612+TheBestPT@users.noreply.github.com>
2021-06-14 22:05:07 +0100

    Fixing some bugs.

--- exemplo_real/src/components/PainelPerguntas.js
+++ exemplo_real/src/components/PainelPerguntas.js
@@ -21,16 +21,16 @@ export default class PainelPerguntas extends React.Component {
                     <h2><b>{this.state.currentQuestion + 1} - {Questions.questions[this.state.currentQuestion].question}</b></h2>
                     <h3>Possible Answer</h3>
                     <div className="questions_center">
-                        <div className="answer" id="1Question" onClick={this.selectQuestion.bind(this)}>
+                        <div className="answer" id="0Question" onClick={this.selectQuestion.bind(this)}>
                             A - {Questions.questions[this.state.currentQuestion].possible_answer[0]}
                         </div>
-                        <div className="answer" id="2Question" onClick={this.selectQuestion.bind(this)}>
+                        <div className="answer" id="1Question" onClick={this.selectQuestion.bind(this)}>
                             B - {Questions.questions[this.state.currentQuestion].possible_answer[1]}
                         </div>
-                        <div className="answer" id="3Question" onClick={this.selectQuestion.bind(this)}>
+                        <div className="answer" id="2Question" onClick={this.selectQuestion.bind(this)}>
                             C - {Questions.questions[this.state.currentQuestion].possible_answer[2]}
                         </div>
-                        <div className="answer" id="4Question" onClick={this.selectQuestion.bind(this)}>
+                        <div className="answer" id="3Question" onClick={this.selectQuestion.bind(this)}>
                             D -{Questions.questions[this.state.currentQuestion].possible_answer[3]}
                         </div>
                     </div>
@@ -64,7 +64,7 @@ export default class PainelPerguntas extends React.Component {
         let id = selector.target.id
         id = id.replace('Question', '')
         let question = this.state.currentQuestion
-        !this.state.answerSelected[question] ? this.state.answerSelected.push(id) : this.state.answerSelected[question] = id
+        !this.state.answerSelected[question] ? this.state.answerSelected.push(id) : this.state.answerSelected[question-1] = id
         console.log(this.state.answerSelected)
         console.log(this.state.answerSelected)
     }
