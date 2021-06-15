a8c3dfd99eb3401a853bc7abf799c16472fd0b98
TheBestPT <72525612+TheBestPT@users.noreply.github.com>
2021-06-15 20:08:01 +0100

    Fix question.

--- exemplo_real/src/components/Questao.js
+++ exemplo_real/src/components/Questao.js
@@ -8,6 +8,7 @@ export default class Questao extends React.Component{
                 <h1>You are a really true fan of Star Wars? Prove yourself!!</h1>
                 <h2>Question {this.props.currentQuestion + 1}</h2>
                 <img src={this.props.images[Questions.questions[this.props.currentQuestion].question_image].default} />
+                <h2><b>{this.props.currentQuestion + 1} - {Questions.questions[this.props.currentQuestion].question}</b></h2>
             </>
         )
     }
