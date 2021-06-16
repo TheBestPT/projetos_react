4bc3b6160c4514e1f00b730b55988d07359dc6d5
TheBestPT <72525612+TheBestPT@users.noreply.github.com>
2021-06-16 22:56:40 +0100

    Trying to solve points and fix the if in render.

--- exemplo_real/src/components/ResultadoPerguntas.js
+++ exemplo_real/src/components/ResultadoPerguntas.js
@@ -10,12 +10,15 @@ export default class ResultadoPerguntas extends React.Component{
                 ++resultado
         }
         //resultado == 0 ? resultado = 0 : --resultado
-        //console.log(resultado)
+        console.log(resultado)
+        //ESTUPIDEZ COMPLETA MAS OLHA VESSE RESOLVES
+        let res = resultado >= questions.length ? (questions.length-1) : resultado
+        //resultado == 5  ? resultado = 4 : resultado
         return (//!= 0 ? resultado+1 : resultado
             <div className="all_content">
                 <h1>Your score {resultado}</h1>
-                <h2>{Questions.question_qualify[resultado == 5 ? resultado = 4 : resultado].phrase}</h2>
-                <img src={this.props.image[Questions.question_qualify[resultado].img].default}/><br />
+                <h2>{Questions.question_qualify[res].phrase}</h2>
+                <img src={this.props.image[Questions.question_qualify[res].img].default}/><br />
                 <button className="button buttons" onClick={() => window.location.reload()}>Play again</button>
             </div>
         )
